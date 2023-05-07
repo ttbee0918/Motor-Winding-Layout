eac35a70913af2da189e22216927a9b879bd8f2d
Ted Tsai <ttbee.tw@gmail.com>
2023-05-07 09:33:29 +0800

    V1.0 基本架構

--- Motor winding 20230501.py
+++ Motor winding 20230501.py
@@ -1,1 +1,36 @@
-import numpy
+import numpy as np
+S = 15
+P = 4
+
+Nph = S/3
+Span = np.floor(S/P)
+E_angle = 180*P/S
+Ncpp = S/P/3
+
+# Initialization
+A = np.zeros((4,S))
+A[0,:] = np.arange(1,S+1).reshape(-1,1).ravel()
+A[1,:] = E_angle*np.arange(0,S).reshape(-1,1).ravel()
+A[2,:] = np.arange(1,S+1).reshape(-1,1).ravel()
+A[3,:] = np.arange(1+Span,S+1+Span).reshape(-1,1).ravel()
+
+for i in range(len(A[3,:])):
+    if A[3,i] > 15:
+        A[3,i] -= 15
+
+# rerange 180 ~ -180
+A[1,:] = np.mod(A[1,:]+180,360)-180
+
+# reverse >90 or <-90
+for i in range(len(A[0,:])):
+    if A[1,i] > 90:
+       A[1,i] = A[1,i] -180
+       A[2,i] = A[3,i]
+       A[3,i] = A[0,i]
+
+    if A[1,i] < -90:
+       A[1,i] = A[1,i] +180
+       A[2,i] = A[3,i]
+       A[3,i] = A[0,i]
+
+print(A)
