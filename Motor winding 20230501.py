2306dce1e44a5134836c82a43c2d936cc250dfb1
Ted Tsai <ttbee.tw@gmail.com>
2023-05-19 07:03:06 +0800

    V1.6 Winding ABC

--- Motor winding 20230501.py
+++ Motor winding 20230501.py
@@ -1,14 +1,16 @@
 import numpy as np
 
 # Initialization
-S = 15                  # Slots number
-P = 4                   # Pole number
+S = 12                  # Slots number
+P = 10                   # Pole number
 Nph = 3           # Phase number
 
 Sph = int(S/Nph)  # Slots per phase
 Span = np.floor(S/P)    # Coil span
 E_angle = 180*P/S       # Electrical angle per slot
 Ncpp = S/P/Nph    # Slots per phase per pole
+
+print('Nominal Coil Span =', Span)
 
 # Calculate K0
 K0 = np.zeros(int(S/2))
@@ -19,7 +21,7 @@ K0 = np.min([int(x) for x in np.nditer(K0) if float(x).is_integer()])
 
 print('K0 = ',K0)
 
-# Phase A
+# Phase A construction
 A = np.zeros((4,S))
 A[0,:] = np.arange(1,S+1).reshape(-1,1).ravel()
 A[1,:] = E_angle*np.arange(0,S).reshape(-1,1).ravel()
@@ -47,41 +49,61 @@ for i in range(len(A[0,:])):
 
 # Sort the 1 column
 A = A[:,np.argsort(np.abs(A[1]),kind='mergesort')]
-
+print(A)
 # Take the phase A
 A = A[:,:Sph].astype(int)
 
-print('Phase A',A)
+print('Phase A =')
+print(A)
 
 # Phase B
 B = A[2:4,:]+K0
 B[B>S] = B[B>S]-S
 
-print('Phase B',B)
+print('Phase B =')
+print(B)
+
+# Phase C
+C = B+K0
+C[C>S] = C[C>S]-S
+
+print('Phase C =')
+print(C)
+
+# Combine
+ABCIn = np.vstack([A[2,:],B[0,:],C[0,:]])
+ABCOut = np.vstack([A[3,:],B[1,:],C[1,:]])
+
+# print("ABCIn = ")
+# print(ABCIn)
+
+# print("ABCOut = ")
+# print(ABCOut)
 
 # Winding
 W = np.empty((S,Nph+1), dtype=object)
 W[:,0] = range(1,S+1)
 
-# Winding A
+# Winding ABC
 for j in range(1,Nph+1):
-    for i in A[2,:]-1:
+    for i in ABCIn[j-1,:]-1:
         if W[i,j] is None:
             W[i,j] = "In"
         else:
             W[i,j] += " & In"
 
-    for i in A[3,:]-1:
+    for i in ABCOut[j-1,:]-1:
         if W[i,j] is None:
             W[i,j] = "Out"
         else:
             W[i,j] += " & Out"
 
+print('Winding = ')
 print(W)
 
 from openpyxl import Workbook
 
-data = A.tolist()
+data = W.tolist()
 
 # 創建 Excel
 workbook = Workbook()
@@ -90,7 +112,7 @@ workbook = Workbook()
 worksheet = workbook.active
 
 # 添加標題行
-headers = ['Coil','Angle', 'In', 'Out']
+headers = ['Slot','Phase A', 'Phase B', 'Phase C']
 
 # 將標題寫入第一行
 for col, header in enumerate(headers, start=1):
