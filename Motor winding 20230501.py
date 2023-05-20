8459cc6c4fcdd1d9e4b80f9283dd5306e120ceb5
Ted Tsai <ttbee.tw@gmail.com>
2023-05-20 12:45:42 +0800

    V1.7 Output to Excel

--- Motor winding 20230501.py
+++ Motor winding 20230501.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 # Initialization
-S = 12                  # Slots number
-P = 10                   # Pole number
+S = 48                  # Slots number
+P = 16                   # Pole number
 Nph = 3           # Phase number
 
 Sph = int(S/Nph)  # Slots per phase
@@ -16,10 +16,12 @@ print('Nominal Coil Span =', Span)
 K0 = np.zeros(int(S/2))
 for q in range(int(S/2)):
     K0[q] = 2*S/3/P*(1+3*q)
+print('Possible K0 =')
+print(K0)
 
+# Choose the smallest as K0
 K0 = np.min([int(x) for x in np.nditer(K0) if float(x).is_integer()])
-
-print('K0 = ',K0)
+print('Choose K0 = ',K0)
 
 # Phase A construction
 A = np.zeros((4,S))
@@ -47,9 +49,12 @@ for i in range(len(A[0,:])):
        A[2,i] = A[3,i]
        A[3,i] = A[0,i]
 
-# Sort the 1 column
+# Sort the phase offset
+A = A[:,np.argsort(A[1],kind='mergesort')]
+
+# Sort the abs phase offset
 A = A[:,np.argsort(np.abs(A[1]),kind='mergesort')]
-print(A)
+
 # Take the phase A
 A = A[:,:Sph].astype(int)
 
@@ -74,12 +79,6 @@ print(C)
 ABCIn = np.vstack([A[2,:],B[0,:],C[0,:]])
 ABCOut = np.vstack([A[3,:],B[1,:],C[1,:]])
 
-# print("ABCIn = ")
-# print(ABCIn)
-
-# print("ABCOut = ")
-# print(ABCOut)
-
 # Winding
 W = np.empty((S,Nph+1), dtype=object)
 W[:,0] = range(1,S+1)
@@ -103,22 +102,25 @@ print(W)
 
 from openpyxl import Workbook
 
-data = W.tolist()
-
 # 創建 Excel
 workbook = Workbook()
 
 # 獲取當前工作表
 worksheet = workbook.active
 
-# 添加標題行
+# 將數據寫入 Excel
+worksheet.append(['Slot',S])
+worksheet.append(['Pole',P])
+worksheet.append(['Phase number',Nph])
+worksheet.append(['Slots per phase',Sph])
+worksheet.append(['Coil span',Span])
+worksheet.append(['Electrical angle per slot',E_angle])
+worksheet.append(['Slots per phase per pole',Ncpp])
+
+# Winding 寫入 Excel
+data = W.tolist()
 headers = ['Slot','Phase A', 'Phase B', 'Phase C']
-
-# 將標題寫入第一行
-for col, header in enumerate(headers, start=1):
-    worksheet.cell(row=1, column=col, value=header)
-
-# 將數據寫入工作表中
+worksheet.append(headers)
 for row in data:
     worksheet.append(row)
 
